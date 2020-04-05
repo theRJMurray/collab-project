6e866dcc7a8c0cf29dc7192fe307f72f1614c1f5
therjmurray <therjmurray@gmail.com>
2020-04-04 23:59:44 -0400

    perform setup ceremony

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./main.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const AppContainer = ({ children }) => {
+	return (
+		<div
+			className="app-container"
+			style={{
+				width: "100%",
+				height: "100%",
+				position: "absolute",
+				top: 0,
+				left: 0,
+				display: "flex"
+			}}
+		>
+			{children}
+		</div>
+	);
+};
+
+const Sidebar = ({ children }) => {
+	return (
+		<div
+			className="sidebar"
+			style={{
+				width: 100,
+				height: "100%",
+				background: "#EEE",
+				borderRight: "1px solid #777"
+			}}
+		>
+			{children}
+		</div>
+	);
+};
+
+const Content = ({ children }) => {
+	return (
+		<div
+			className="content"
+			style={{ width: "100%", height: "100%", background: "#C0FFEE" }}
+		>
+			{children}
+		</div>
+	);
+};
+
+const App = () => {
+	return (
+		<AppContainer>
+			<Sidebar />
+			<Content />
+		</AppContainer>
+	);
+};
 
 export default App;
